383ec527e3dde49f7f04f7e46d8def863328c44c
JemJoestar <jemapro2@gmail.com>
2023-05-23 16:46:35 +0300

    header

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,32 +1,23 @@
 (() => {
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
-  const closeMenuBtn = document.querySelector('.js-close-menu');
-
+  const closeMenuBtn = document.querySelectorAll('.js-close-menu');
   const toggleMenu = () => {
-    const isMenuOpen =
-      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-    mobileMenu.classList.toggle('is-open');
-    openMenuBtn.classList.toggle('hidden');
-    closeMenuBtn.classList.toggle('hidden');
-
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
+     const isMenuOpen =
+        openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+     mobileMenu.classList.toggle('is-open');
   };
-
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
-
+  closeMenuBtn.forEach((link) => link.addEventListener("click", toggleMenu));
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-    if (!e.matches) return;
-    mobileMenu.classList.remove('is-open');
-    openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
-    closeMenuBtn.classList.toggle('hidden');
-    openMenuBtn.classList.toggle('hidden');
+     if (!e.matches) return;
+     mobileMenu.classList.remove('is-open');
+     openMenuBtn.setAttribute('aria-expanded', false);
+     bodyScrollLock.enableBodyScroll(document.body);
+     closeMenuBtn.classList.toggle('hidden');
+     openMenuBtn.classList.toggle('hidden');
   });
 })();
